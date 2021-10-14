9ce0ea2baeca54cbc1718d4992ee387d4df84fc5
NITSlN <nitishgoku01@gmail.com>
2021-10-14 16:24:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+app.get('/',(req,res)=>{
+    res.send("ballu")
+})
+
+
+app.listen(PORT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
